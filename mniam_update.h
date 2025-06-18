135d4d312627a1fb46ebc555cd8891d08b66154c
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-06-18 10:51:07 +0200

    Update mniam_update.h
    upadte include updated

--- mniam_update.h
+++ mniam_update.h
@@ -1,8 +1,22 @@
-#ifndef MNIAM_UPDATE_H_
-#define MNIAM_UPDATE_H_
+/*
+ * mniam_update.h
+ *
+ *  Created on: Jun 16, 2025
+ *      Author: Hubert
+ */
+
+#ifndef INC_MNIAM_UPDATE_H_
+#define INC_MNIAM_UPDATE_H_
 
 #include "amcom_packets.h"
 
+/**
+ * Saves object to chosen List
+ *
+ * @param List pointer to the list where the object will be stored
+ * @param object pointer to the AMCOM_ObjectState structure to the object to be stored
+ * #param size size of the List
+*/
 void SaveObject(AMCOM_ObjectState* List, const AMCOM_ObjectState* object, size_t size);
 
-#endif
+#endif /* INC_MNIAM_UPDATE_H_ */
