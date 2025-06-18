a262e56b0197f469a10e0b1db68e56edeede0cdb
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-06-18 10:49:49 +0200

    Update mniam_update.c
    update updated

--- mniam_update.c
+++ mniam_update.c
@@ -1,3 +1,10 @@
+/*
+ * mniam_update.c
+ *
+ *  Created on: Jun 16, 2025
+ *      Author: Hubert
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 
@@ -5,10 +12,10 @@
 
 void SaveObject(AMCOM_ObjectState* List, const AMCOM_ObjectState* object, size_t size){
     uint8_t updated = 0;
-    
+
     for(size_t j = 0; j < size; j++) {
-                if(List[j].objectType == object->objectType && List[j].objectNo == object->objectNo ) {
-                    if(object->hp == 0){
+                if(List[j].objectType == object->objectType && List[j].objectNo == object->objectNo ) {		//condition checking if the object is already saved in the list
+                    if(object->hp == 0){																	//condition determining whether object is dead or should be updated
                         List[j].objectType = 5;
                         List[j].hp = 0;
                     } else {
