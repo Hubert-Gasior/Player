9ae729133074a4bd493f07ecb58da0acb5c1dbff
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-06-18 10:48:31 +0200

    Update mniam_move.c
    move updated

--- mniam_move.c
+++ mniam_move.c
@@ -1,3 +1,10 @@
+/*
+ * mniam_move.c
+ *
+ *  Created on: Jun 16, 2025
+ *      Author: Hubert
+ */
+
 #include <string.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -9,6 +16,18 @@ float CalculatePath(const position* object_1, const position* object_2) {
 	return ((object_2->x - object_1->x)*(object_2->x - object_1->x)) + ((object_2->y - object_1->y)*(object_2->y - object_1->y));
 }
 
+float CalculateAngle(const position* StartPosition, const position* EndPosition){
+    float angle = 0;
+    float deltaX = EndPosition->x - StartPosition->x;
+    float deltaY = EndPosition->y - StartPosition->y;
+
+    angle = atan2(deltaY, deltaX);
+    if(angle < 0){
+        angle += (2 * M_PI);
+    }
+    return angle;
+}
+
 uint8_t IsAlive(const AMCOM_ObjectState* ObjectList, const position* object, size_t size){
     for(size_t i = 0; i < size; i++){
         if(ObjectList[i].x == object->x && ObjectList[i].y == object->y && ObjectList[i].hp != 0){
@@ -18,13 +37,13 @@ uint8_t IsAlive(const AMCOM_ObjectState* ObjectList, const position* object, siz
     return 0;
 }
 
-AMCOM_ObjectState FindPosition(const AMCOM_ObjectState* ObjectList, position* Object, uint8_t ObjectNumber, uint8_t ObjectType, size_t size) {
+AMCOM_ObjectState FindPosition(const AMCOM_ObjectState* ObjectList, position* Object, uint8_t ObjectNo, uint8_t ObjectType, size_t size) {
 	AMCOM_ObjectState FoundObject;
     FoundObject.hp = 0;
 
     for (size_t i = 0; i < size; i++) {
-		if(ObjectList[i].objectType == ObjectType && ObjectList[i].objectNo == ObjectNumber) {
-            FoundObject = ObjectList[i]; 
+		if(ObjectList[i].objectType == ObjectType && ObjectList[i].objectNo == ObjectNo) {
+            FoundObject = ObjectList[i];
 			Object->x = ObjectList[i].x;
 			Object->y = ObjectList[i].y;
 			break;
@@ -67,95 +86,35 @@ AMCOM_ObjectState FindClosestObjectOfType(const AMCOM_ObjectState* ObjectList, c
                     SecondPath = path;
                 }
             }
-        }  
+        }
     }
 
 	return ClosestObject;
 }
 
-float CalculateAngle(const position* StartPosition, const position* EndPosition){
-    float angle = 0;
-    float deltaX = EndPosition->x - StartPosition->x;
-    float deltaY = EndPosition->y - StartPosition->y;
-    angle = atan2(deltaY, deltaX);
-    if(angle < 0){
-        angle += (2 * M_PI);
-    }
-    return angle;
-}
-
-float obstacle(const AMCOM_ObjectState* ObstacleList, const position* StartPosition , const position* EndPosition, const AMCOM_ObjectState* Player, uint8_t diameter, size_t size){
-    float Path = CalculatePath(StartPosition, EndPosition);
-    float PlayerRadius = (25.0 + Player->hp)/2.0;
-    float ObstacleRadius = diameter/2.0;
-    float CombinedRadius = ObstacleRadius + PlayerRadius;
-    position StartEnd;
-    position StartCenter;
-    position CurrentObstacle;
-    position ClosestPoint;
-    float StartEndSquaredLength = 0;
-    float MinDistanceToCenter = 0;
-    float Overlap = 0;
-    float Chord = 0;
-    float t = 0;
-
-    StartEnd.x = EndPosition->x - StartPosition->x;
-    StartEnd.y = EndPosition->y - StartPosition->y;
-    
-
-    for(size_t i = 0; i < size; i++){
-        CurrentObstacle.x = ObstacleList[i].x;
-        CurrentObstacle.y = ObstacleList[i].y;
-        StartCenter.x = ObstacleList[i].x - StartPosition->x;
-        StartCenter.y = ObstacleList[i].y - StartPosition->y;
-
-        StartEndSquaredLength = (StartEnd.x * StartEnd.x) + (StartEnd.y * StartEnd.y);
-        t = ((StartEnd.x*StartCenter.x) + (StartEnd.y * StartCenter.y))/StartEndSquaredLength;
-
-        if (t < 0.0f) t = 0.0f;
-        else if (t > 1.0f) t = 1.0f;
-
-        ClosestPoint.x = StartPosition->x + StartEnd.x * t;
-        ClosestPoint.x = StartPosition->y + StartEnd.y * t;
-
-        MinDistanceToCenter = CalculatePath(&CurrentObstacle, &ClosestPoint);
-
-        if(MinDistanceToCenter < CombinedRadius){
-            Chord = 2.0 * sqrtf(CombinedRadius * CombinedRadius - MinDistanceToCenter * MinDistanceToCenter);
-        }
-
-        Overlap += Chord;
-
-        return Overlap/Path;
-    }
-}
-
-void CalculateAlternativePaths(const AMCOM_ObjectState* List, const position* StartPosition, position* EndPosition, size_t size, const AMCOM_ObjectState* GlueList, const AMCOM_ObjectState* Player, size_t GlueListSize){
+void CalculateAlternativePaths(const AMCOM_ObjectState* List, const position* StartPosition, position* EndPosition, size_t size){
     position FirstObject;
     position SecondObject;
     position current;
     position prev;
     float FirstPath = 0;
     float SecondPath = 0;
-    float multiplier = 0;
 
     FindClosestObjectOfType(List, StartPosition, &FirstObject, TRANSISTOR, size, 0);
     FindClosestObjectOfType(List, StartPosition, &SecondObject, TRANSISTOR, size, 1);
 
     prev = *StartPosition;
     current = FirstObject;
-    for(size_t i = 0; i < 3; i++){
-        multiplier = obstacle(GlueList, &prev, &current, Player, GLUE_DIAMETER, GlueListSize);
-        FirstPath += (1.0 + multiplier * 20) + CalculatePath(&prev, &current);
+    for(size_t i = 0; i < 3; i++){														//loop calculating sum of the distance for 3 next steps starting from first closest transistor
+        FirstPath += CalculatePath(&prev, &current);
         prev = current;
         FindClosestObjectOfType(List, &prev, &current, TRANSISTOR, size, 0);
     }
 
     prev = *StartPosition;
     current = SecondObject;
-    for(size_t i = 0; i < 3; i++){
-        multiplier = obstacle(GlueList, &prev, &current, Player, GLUE_DIAMETER, GlueListSize);
-        SecondPath += (1.0 + multiplier * 20) + CalculatePath(&prev, &current);
+    for(size_t i = 0; i < 3; i++){														//loop calculating sum of the distance for 3 next steps starting from second closest transistor
+        SecondPath += CalculatePath(&prev, &current);
         prev = current;
         FindClosestObjectOfType(List, &prev, &current, TRANSISTOR, size, 0);
     }
@@ -163,8 +122,36 @@ void CalculateAlternativePaths(const AMCOM_ObjectState* List, const position* St
     if(FirstPath < SecondPath){
         *EndPosition = FirstObject;
     }
-    
+
     *EndPosition = SecondObject;
+}
+
+float AvoidObstacle(const AMCOM_ObjectState* ObstacleList, const AMCOM_ObjectState* Player, uint8_t diameter, size_t size){
+    float PlayerRadius = (25.0 + Player->hp)/2.0;
+    float ObstacleRadius = diameter/2.0;
+    float CombinedRadius = PlayerRadius + ObstacleRadius + 50;		//added 50 for safety
+    float DistanceToObstacle = 0.0;
+    position ObstaclePosition;
+    position PlayerPosition;
+    float ratio = 0.0;
+
+    PlayerPosition.x = Player->x;
+    PlayerPosition.y = Player->y;
+
+    for(size_t i = 0; i < size; i++){
+        if(ObstacleList[i].hp == 0){
+            continue;
+        }
+        ObstaclePosition.x = ObstacleList[i].x;
+        ObstaclePosition.y = ObstacleList[i].y;
+        DistanceToObstacle = sqrt(CalculatePath(&PlayerPosition, &ObstaclePosition));
+        if(DistanceToObstacle > CombinedRadius){
+            continue;
+        }
+
+            ratio += DistanceToObstacle/CombinedRadius;
+    }
+    return ratio;
 }
 
 AMCOM_ObjectState FindBestTarget(const AMCOM_ObjectState* ObjectList, const AMCOM_ObjectState* Player, position* Target, size_t size){
